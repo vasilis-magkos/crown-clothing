c75ec4adfc582a83cbcaf0d103312d9197e7e647
vasilis-magkos <vmagkos@dckdigital.com>
2023-12-08 11:43:07 +0000

    latest changes

--- src/Components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/Components/cart-dropdown/cart-dropdown.styles.jsx
@@ -4,7 +4,7 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
-} from '../button/button.styles';
+} from '../../components/button/button.styles';
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
